0a093a89cd4af694efcbc2d692b327ad0c808a69
andrew stehno <andrew_stehno@yahoo.com>
2021-03-20 23:56:37 -0600

    updated index.html, webpack and dev-server running

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundled.js',
+        path: path.resolve(__dirname, 'app'),
+    },
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html')
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+    },
+    mode: 'development'
+}
