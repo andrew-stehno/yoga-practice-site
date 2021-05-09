7fb05ef0c49590f7bdde621f3bfc9ec269eb0931
andrew stehno <andrew_stehno@yahoo.com>
2021-05-09 17:39:31 -0600

    adjusting styling to mobile menu links

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,21 +1,21 @@
 class MobileMenu {
     constructor() {
-    //     document.addEventListener("click", () => {document.querySelector(".site-header__menu-icon");
-    //     document.querySelector(".site-header__menu-content").classList.toggle("site-header__menu-content--is-visible");
-    // })
+        document.addEventListener("click", () => {document.querySelector(".site-header__menu-icon");
+        document.querySelector(".site-header__menu-content").classList.toggle("site-header__menu-content--is-visible");
+    })
 
-        this.menuIcon = document.querySelector(".site-header__menu-icon")
-        this.menuContent = document.querySelector(".site-header__menu-content")
-        this.events()
+        // this.menuIcon = document.querySelector(".site-header__menu-icon")
+        // this.menuContent = document.querySelector(".site-header__menu-content")
+        // this.events()
     }
 
-    events() {
-        this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
-    }
+    // events() {
+    //     this.menuIcon.addEventListener("click", () => this.toggleTheMenu())
+    // }
 
-    toggleTheMenu() {
-        this.menuContent.classList.toggle("site-header__menu-content--is-visible")
-    }
+    // toggleTheMenu() {
+    //     this.menuContent.classList.toggle("site-header__menu-content--is-visible")
+    // }
  
 }
 
