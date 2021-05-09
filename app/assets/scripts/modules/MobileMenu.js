7d1339e84f1e99868637be39645e3b60a4dd65e4
andrew stehno <andrew_stehno@yahoo.com>
2021-05-08 21:16:44 -0600

    troubleshooting js issue with MobileMenu constructor function

--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -1,5 +1,9 @@
 class MobileMenu {
     constructor() {
+    //     document.addEventListener("click", () => {document.querySelector(".site-header__menu-icon");
+    //     document.querySelector(".site-header__menu-content").classList.toggle("site-header__menu-content--is-visible");
+    // })
+
         this.menuIcon = document.querySelector(".site-header__menu-icon")
         this.menuContent = document.querySelector(".site-header__menu-content")
         this.events()
@@ -12,6 +16,7 @@ class MobileMenu {
     toggleTheMenu() {
         this.menuContent.classList.toggle("site-header__menu-content--is-visible")
     }
+ 
 }
 
 export default MobileMenu;
